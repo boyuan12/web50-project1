3d08a2331812e9546f3452bc1339e5e0021a9051
boyuan12 <boyuanliu6@yahoo.com>
2020-02-09 08:35:44 -0800

    Update application.py

--- project1/application.py
+++ project1/application.py
@@ -14,17 +14,13 @@ from helper import login_required
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgres://badljuvscpblkv:77846dcebffb03ab3ab9785e08fd5c02ab5f376630655de1c84ad65088ce4931@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d5loculdplltmr")
 db = scoped_session(sessionmaker(bind=engine))
 s = db()
 
